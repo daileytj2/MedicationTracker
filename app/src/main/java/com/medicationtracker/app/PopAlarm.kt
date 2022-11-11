5adb57d896eb1f69fe25b976e45409bab06c078c
daileytj2 <54542601+daileytj2@users.noreply.github.com>
2022-11-11 14:59:55 -0500

    Replacing Alarm function

---
+++ app/src/main/java/com/medicationtracker/app/PopAlarm.kt
@@ -0,0 +1,39 @@
+package com.medicationtracker.app
+
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+
+class PopAlarm : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var myView= inflater.inflate(R.layout.pop_alarm, container, false)
+
+        var btnAlarmDone=myView.findViewById(R.id.btnAlarmDone) as Button
+        var timePicker=myView.findViewById(R.id.timePicker) as TimePicker
+
+        btnAlarmDone.setOnClickListener {
+
+            val ma = activity as MainActivity
+            if (Build.VERSION.SDK_INT >= 23) {
+                ma.SetTime(timePicker.hour, timePicker.minute)
+            } else {
+                ma.SetTime(timePicker.currentHour, timePicker.currentMinute)
+            }
+
+            this.dismiss()
+        }
+
+        return myView
+    }
+}
