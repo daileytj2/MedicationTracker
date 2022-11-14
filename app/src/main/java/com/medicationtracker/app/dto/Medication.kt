0bbcdcc9673aa2b8b055bd87c14d2d6712d3dba3
Christopher Elbe <elbecr@mail.uc.edu>
2022-11-14 14:52:42 -0500

    Medication DTO additions

--- app/src/main/java/com/medicationtracker/app/dto/Medication.kt
+++ app/src/main/java/com/medicationtracker/app/dto/Medication.kt
@@ -1,6 +1,6 @@
 package com.medicationtracker.app.dto
 
-data class Medication(var id : String = "", var name: String = "", var doseAmount : Int = 0){
+data class Medication(var id : String = "", var name: String = "", var doseAmount : Int = 0, var RXNumber : String = "", var expDate : String = ""){
     override fun toString(): String {
         return "$name $doseAmount"
     }
