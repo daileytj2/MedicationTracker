07f1ba11b009c1f42b63d6df3bfa35bef4f739aa
daileytj2 <54542601+daileytj2@users.noreply.github.com>
2023-01-29 18:28:25 -0500

    Alarm functionality

---
+++ app/src/main/java/com/medicationtracker/app/service/AlarmService.kt
@@ -0,0 +1,82 @@
+package com.medicationtracker.app.service
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import com.medicationtracker.app.AlarmReceiver
+import com.medicationtracker.app.util.Constants
+import com.medicationtracker.app.util.RandomIntUtil
+
+class AlarmService(private val context: Context) {
+    private val alarmManager: AlarmManager? =
+        context.getSystemService(Context.ALARM_SERVICE) as AlarmManager?
+
+    fun setExactAlarm(timeInMillis: Long){
+        setAlarm(
+            timeInMillis,
+            getPendingIntent(
+                getIntent().apply {
+                    action = Constants.ACTION_SET_EXACT_ALARM
+                    putExtra(Constants.EXTRA_EXACT_ALARM_TIME, timeInMillis)
+                }
+            )
+        )
+    }
+
+    //Every week
+    fun setWeeklyAlarm(timeInMillis: Long){
+        setAlarm(
+            timeInMillis,
+            getPendingIntent(
+                getIntent().apply {
+                    action = Constants.ACTION_SET_REPETITIVE_WEEKLY_ALARM
+                    putExtra(Constants.EXTRA_EXACT_ALARM_TIME, timeInMillis)
+                }
+            )
+        )
+    }
+
+    //Every day
+    fun setDailyAlarm(timeInMillis: Long){
+        setAlarm(
+            timeInMillis,
+            getPendingIntent(
+                getIntent().apply {
+                    action = Constants.ACTION_SET_REPETITIVE_DAILY_ALARM
+                    putExtra(Constants.EXTRA_EXACT_ALARM_TIME, timeInMillis)
+                }
+            )
+        )
+    }
+
+    private fun setAlarm(timeInMillis: Long, pendingIntent: PendingIntent){
+
+        alarmManager?.let {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                alarmManager.setExactAndAllowWhileIdle(
+                    AlarmManager.RTC_WAKEUP,
+                    timeInMillis,
+                    pendingIntent
+                )
+            }else {
+                alarmManager.setExact(
+                    AlarmManager.RTC_WAKEUP,
+                    timeInMillis,
+                    pendingIntent
+                )
+            }
+        }
+    }
+
+    private fun getIntent() = Intent(context, AlarmReceiver::class.java)
+
+    private fun getPendingIntent(intent: Intent) =
+        PendingIntent.getBroadcast(
+            context,
+            RandomIntUtil.getRandomInt(),
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+}
