fcf6017c62d0af35c9612414c57c8de41ab08bff
lcarlsenl <76413684+lcarlsenl@users.noreply.github.com>
2023-02-18 16:12:15 -0500

    UI changes

--- app/src/main/java/com/medicationtracker/app/Login.kt
+++ app/src/main/java/com/medicationtracker/app/Login.kt
@@ -1,10 +1,7 @@
 package com.medicationtracker.app
 
 import android.content.Intent
-import android.hardware.biometrics.BiometricManager.Authenticators.BIOMETRIC_STRONG
-import android.hardware.biometrics.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
@@ -14,59 +11,56 @@ import kotlinx.android.synthetic.main.login.*
 import java.util.concurrent.Executor
 
 class Login : AppCompatActivity(){
-    lateinit var executor: Executor
-    private lateinit var biometricPrompt: BiometricPrompt
-    private lateinit var promptInfo: BiometricPrompt.PromptInfo
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
-        executor = ContextCompat.getMainExecutor(this)
-        biometricPrompt = BiometricPrompt(this@Login, executor,
-            object : BiometricPrompt.AuthenticationCallback() {
-                //override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                //    super.onAuthenticationError(errorCode, errString)
-                //    setContentView(R.layout.login)
-                //    loginnote.text = "Authentication Error"
-                //    Toast.makeText(this@Login,
-                //        "Authentication Error $errString",
-                //        Toast.LENGTH_SHORT
-                //    )
-                //        .show()
-                //}
 
-                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-                    super.onAuthenticationSucceeded(result)
-                    loginnote.text = "Authentication Success!"
-                    Toast.makeText(this@Login, "Authentication Success!", Toast.LENGTH_SHORT)
-                        .show()
+
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.login)
+            var executor: Executor = ContextCompat.getMainExecutor(this)
+            var biometricPrompt: BiometricPrompt = BiometricPrompt(this@Login, executor,
+                object : BiometricPrompt.AuthenticationCallback() {
+
+
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(result)
+                        loginnote.text = "Authentication Success!"
+                        Toast.makeText(this@Login, "Authentication Success!", Toast.LENGTH_SHORT)
+                            .show()
                         val intent = Intent(this@Login, MainActivity::class.java)
                         // start your next activity
                         startActivity(intent)
 
-                }
+                    }
 
-                override fun onAuthenticationFailed() {
-                    super.onAuthenticationFailed()
-                    loginnote.text = "Authentication Failed"
-                    Toast.makeText(this@Login, "Authentication Failed", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            })
-        promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Biometric Authentication")
-            .setSubtitle("Login using fingerprint authentication")
-            //.setNegativeButtonText("Use Password instead")
-            .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
-            .setConfirmationRequired(false)
-            .build()
+                    override fun onAuthenticationFailed() {
+                        super.onAuthenticationFailed()
+                        loginnote.text = "Authentication Failed"
+                        Toast.makeText(this@Login, "Authentication Failed", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                })
 
-        login.setOnClickListener()
-        {
-            biometricPrompt.authenticate(promptInfo)
+        if (BiometricManager.from(this).canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL) == BiometricManager.BIOMETRIC_SUCCESS) {
+            login.setOnClickListener()
+            {
+                var promptInfo: BiometricPrompt.PromptInfo = BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric Authentication")
+                    .setSubtitle("Login using fingerprint authentication")
+                    .setAllowedAuthenticators(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                    .setConfirmationRequired(false)
+                    .build()
+                biometricPrompt.authenticate(promptInfo)
 
 
+            }
+        }
+        else{
+            login.setOnClickListener()
+            {
+                val login = Intent(this@Login, MainActivity::class.java)
+                startActivity(login)
+            }
         }
     }
 }
--- app/src/main/java/com/medicationtracker/app/MainActivity.kt
+++ app/src/main/java/com/medicationtracker/app/MainActivity.kt
@@ -1,25 +1,16 @@
 package com.medicationtracker.app
 
-import android.app.DatePickerDialog
-import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import android.telephony.SmsManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.medicationtracker.app.service.AlarmService
 import kotlinx.android.synthetic.main.activity_addmedication.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.login.*
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import kotlinx.android.synthetic.main.mainidea.*
 import java.util.*
-import java.util.concurrent.Executor
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,182 +19,27 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.mainidea)
         alarmService =AlarmService(this)
 
-        val loginpage = findViewById<Button>(R.id.tologin)
-        loginpage.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            // start your next activity
-            startActivity(intent)
-        }
-        val new = findViewById<Button>(R.id.notificationpage)
-        new.setOnClickListener {
-            val intent = Intent(this, Notification::class.java)
-            // start your next activity
-            startActivity(intent)
-        }
-        //Values for email and phone number
-        val rejectButton = findViewById<Button>(R.id.btnReject)
-        val acceptButton = findViewById<Button>(R.id.btnAccept)
-        val emailAddress = findViewById<EditText>(R.id.etEmailAddress)
-        val phoneNumber = findViewById<EditText>(R.id.etPhoneNumber)
-        val smsManager:SmsManager = SmsManager.getDefault()
-        val current = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-        val formatted = current.format(formatter)
-
-
-        //Values for alarm
-//        val btnSetAlarm = findViewById<Button>(R.id.btnAlarm)
-//        val txtShowTime = findViewById<TextView>(R.id.txtShowTime)
-
-        btnWeeklyAlarm.setOnClickListener{
-            setAlarm{ alarmService.setWeeklyAlarm(it) }
-        }
-
-        btnDailyAlarm.setOnClickListener{
-            setAlarm{ alarmService.setDailyAlarm(it) }
-        }
-
-        //Reject Button
-        rejectButton.setOnClickListener{
-
-            var message = "The patient has not taken their medication."
-            var address = emailAddress.text.toString()
-            var subject = "MedTracker"
-            var phone = phoneNumber.text.toString()
-
-
-            if (address == "" && phone == ""){
-                Toast.makeText(
-                    this@MainActivity,
-                    "Please, enter email address or phone number!",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }else {
-                //phone
-                smsManager.sendTextMessage(phone, null, message, null, null)
-
-                //email
-//                val intent = Intent(Intent.ACTION_SEND)
-//                intent.putExtra(Intent.EXTRA_EMAIL, address)
-//                intent.putExtra(Intent.EXTRA_SUBJECT, subject)
-//                intent.putExtra(Intent.EXTRA_TEXT, message)
-//                intent.type = "message/rfc822"
-//                startActivity(Intent.createChooser(intent, "Select email"))
-
-                //successful
-                Toast.makeText(
-                    this@MainActivity,
-                    "Message sent",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-
-        }
-
-        //Accept Button
-        acceptButton.setOnClickListener{
-
-            var message = "The patient has taken their medication on $formatted."
-            var address = emailAddress.text.toString()
-            var subject = "MedTracker"
-            var phone = phoneNumber.text.toString()
-
-
-            if (address == "" && phone == ""){
-                Toast.makeText(
-                    this@MainActivity,
-                    "Please, enter email address or phone number!",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }else {
-                //phone
-                smsManager.sendTextMessage(phone, null, message, null, null)
-
-                //email
-//                val intent = Intent(Intent.ACTION_SEND)
-//                intent.putExtra(Intent.EXTRA_EMAIL, address)
-//                intent.putExtra(Intent.EXTRA_SUBJECT, subject)
-//                intent.putExtra(Intent.EXTRA_TEXT, message)
-//                intent.type = "message/rfc822"
-//                startActivity(Intent.createChooser(intent, "Select email"))
-
-                //successful
-                Toast.makeText(
-                    this@MainActivity,
-                    "Message sent",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-
-        }
-
-        btnAddMed.setOnClickListener(){
-            val addMedScreen = Intent(this@MainActivity, AddMedication::class.java)
+        tomedication.setOnClickListener(){
+            val addMedScreen = Intent(this, AddMedication::class.java)
             startActivity(addMedScreen)
         }
-
-        btnAddContact.setOnClickListener {
-            val addContactScreen = Intent(this@MainActivity, AddContact::class.java)
+        toalarms.setOnClickListener(){
+            val addAlarmScreen = Intent(this, AlarmPage::class.java)
+            startActivity(addAlarmScreen)
+        }
+        tocontacts.setOnClickListener(){
+            val addContactScreen = Intent(this, AddContact::class.java)
             startActivity(addContactScreen)
         }
-
-        btnViewMedications.setOnClickListener {
-            val addViewMedicationScreen = Intent(this@MainActivity, DisplayMedication::class.java)
-            startActivity(addViewMedicationScreen)
+        tohistory.setOnClickListener(){
+            val addHistoryScreen = Intent(this, DisplayMedication::class.java)
+            startActivity(addHistoryScreen)
         }
     }
-
-//    fun btnSetAlarm(view:View){
-//        val popAlarm= PopAlarm()
-//        val fm=supportFragmentManager
-//        popAlarm.show(fm,"Select time")
-//    }
-
-    private fun setAlarm(callback: (Long) -> Unit) {
-        Calendar.getInstance().apply {
-            this.set(Calendar.SECOND,0)
-            this.set(Calendar.MILLISECOND,0)
-            DatePickerDialog(
-                this@MainActivity,
-                0,
-                {_, year, month, day ->
-                    this.set(Calendar.YEAR, year)
-                    this.set(Calendar.MONTH, month)
-                    this.set(Calendar.DAY_OF_MONTH, day)
-
-                    TimePickerDialog(
-                        this@MainActivity,
-                        0,
-                        {_, hour, min ->
-                            this.set(Calendar.HOUR_OF_DAY, hour)
-                            this.set(Calendar.MINUTE, min)
-                            callback(this.timeInMillis)
-                        },
-                        this.get(Calendar.HOUR_OF_DAY),
-                        this.get(Calendar.MINUTE),
-                        false
-                    ).show()
-                },
-                this.get(Calendar.YEAR),
-                this.get(Calendar.MONTH),
-                this.get(Calendar.DAY_OF_MONTH)
-
-            ).show()
-        }
-    }
-
-
-    fun setTime(Hours:Int,Minute:Int){
-
-        txtShowTime.text= "$Hours:$Minute"
-
-        val saveAlarmData=SaveAlarmData(applicationContext)
-        saveAlarmData.SaveData(Hours, Minute)
-        saveAlarmData.setAlarm()
-    }
-
 
 }
+
+
